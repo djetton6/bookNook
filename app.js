7751524b9f0fdadb0f2f9fec037b6c794143651e
Desmend Jetton <djetton@iit.edu>
2019-01-22 19:58:26 -0600

    DOM manipulation based on button click works.

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-BookNook.js;
+console.log("testing app under construction");
+let submitButton = document.getElementById("submitButton");
+let bookResults = document.getElementsByClassName("book-results");
+
+submitButton.addEventListener("click", testingButton);
+function testingButton() {
+  document.getElementById("title").innerHTML = "Hello World";
+}
