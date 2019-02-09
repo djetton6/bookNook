6f0af5011b6ccf62c89cceff47da046c1a43da1b
Desmend Jetton <djetton@iit.edu>
2019-02-08 18:49:00 -0600

    removed unused variables from application.

--- app.js
+++ app.js
@@ -6,30 +6,35 @@ $(document).ready(function() {
     if (search == "") {
       alert("Please enter a book to search");
     } else {
-      console.log("testing");
       //Return all the API elements for query
-      let img = "";
-      let title = "";
-      let authors = "";
-      let publisher = "";
 
       $.get("https://www.googleapis.com/books/v1/volumes?q=" + search, function(
         response
       ) {
-        console.log(response);
-        const searchResultsDiv = document.querySelector('#search-results');
+        const searchResultsDiv = document.querySelector("#search-results");
         //Loop over JSON to show book properties
         for (i = 0; i < response.items.length; i++) {
           const item = response.items[i];
-          const itemDiv = document.createElement('div');
+          const itemDiv = document.createElement("div");
           itemDiv.innerHTML = `<div>
-                                <h1>${item.volumeInfo.title}</h1>
-                                <img src=${item.volumeInfo.imageLinks.thumbnail} />
-                                ${item.volumeInfo.authors.length &&  '<b>Authors:</b> <ul>' + item.volumeInfo.authors.map(author => {
-                                  return `<li>${author}</li>`;
-                                }).join('') + '</ul>'}
+                                <h2 id="book-title">${
+                                  item.volumeInfo.title
+                                }</h2>
+                                <img src=${
+                                  item.volumeInfo.imageLinks.thumbnail
+                                } />
+                                ${item.volumeInfo.authors.length &&
+                                  "<b>Authors:</b> <ul>" +
+                                    item.volumeInfo.authors
+                                      .map(author => {
+                                        return `<li>${author}</li>`;
+                                      })
+                                      .join("") +
+                                    "</ul>"}
                                 <b>Publisher:</b> ${item.volumeInfo.publisher}
-                                <a target=_blank href=${item.volumeInfo.previewLink} >Learn More...</a>
+                                <a target=_blank id="more-info" href=${
+                                  item.volumeInfo.previewLink
+                                } >Learn More...</a></button>
                               </div>`;
           searchResultsDiv.append(itemDiv);
         }
