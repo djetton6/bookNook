ec8644d3e382959f32860eaf229c82f0c6d0844e
Desmend Jetton <djetton@iit.edu>
2019-02-08 23:58:32 -0600

    added test field using QUnit and display change.

---
+++ test.js
@@ -0,0 +1,13 @@
+import { ok } from "assert";
+
+/*
+it should:
+    return the title of the book
+    return the authors of the book
+    return the publishing house of the book
+    return the image of the book
+*/
+
+test("Title is loaded correctly", function() {
+    ok(response.items)
+})
